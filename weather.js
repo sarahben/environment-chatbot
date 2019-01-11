8b0a042af53624fe13d7523a22d8c95aaa725c06
Sarah <sarah.benfdil@gmail.com>
2019-01-11 15:02:59 +0100

    com

--- weather.js
+++ weather.js
@@ -1,19 +1,48 @@
-const request = require('request')
+// const request = require('request');
+//
+// const xml = './Enveloppe.xml';
+// const opts = {
+//     body: xml,
+//     headers: {
+//         'Content-Type': 'text/xml; charset=utf-8',
+//         SOAPAction: 'runTransaction'
+//     },
+//     wsdl_options: {
+//         proxy: process.env.QUOTAGUARDSTATIC_URL
+//     }
+// }
+// console.log(xml);
+// const url = 'http://statutvolp.royalairmaroc.com/WebServiceStatutDeVol/services/FlightStatus?wsdl';
+//
+// const body = request.post(url, opts, (err, response) => {
+//     console.log('response', response.body);
+// })
 
-const xml = './Enveloppe.xml'
-const opts = {
-    body: xml,
-    headers: {
-        'Content-Type': 'text/xml; charset=utf-8',
-        SOAPAction: 'runTransaction'
-    },
-    wsdl_options: {
-        proxy: process.env.QUOTAGUARDSTATIC_URL
-    }
-}
 
-const url = 'http://statutvolp.royalairmaroc.com/WebServiceStatutDeVol/services/FlightStatus?wsdl'
+// access an HTTP API
+'use strict';
+var http, options, proxy, url, target;
 
-const body = request.post(url, opts, (err, response) => {
-    console.log('response', response.body)
-})
+http = require("http");
+url = require("url");
+
+let quota = "http://38tzc6v3ms43ku:rT4elgo_oPu3fsO3sUhusgt_uQ@eu-west-static-01.quotaguard.com:9293";
+
+proxy = url.parse(quota);
+target  = url.parse("http://statutvolp.royalairmaroc.com/WebServiceStatutDeVol/services/FlightStatus?wsdl");
+
+options = {
+  hostname: proxy.hostname,
+  port: proxy.port || 80,
+  path: target.href,
+  headers: {
+    "Proxy-Authorization": "Basic " + (new Buffer(proxy.auth).toString("base64")),
+    "Host" : target.hostname
+  }
+};
+console.log(proxy);
+
+http.get(options, function(res) {
+  res.pipe(process.stdout);
+  return console.log("status code", res.statusCode);
+  });
