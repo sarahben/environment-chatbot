00d9c732885a3473c71a5988642053ec2e0c77a8
Sarah <sarah.benfdil@gmail.com>
2019-01-11 12:53:46 +0100

    weather

--- weather.js
+++ weather.js
@@ -1,67 +1,73 @@
-// access an HTTP API
-var http, options, proxy, url;
-
-http = require("http");
-url = require("url");
-
-let quota = process.env.QUOTAGUARDSTATIC_URL";
-
-proxy = url.parse(quota);
-target  = url.parse("http://statutvolp.royalairmaroc.com/WebServiceStatutDeVol/services/FlightStatus?wsdl");
-
-options = {
-  hostname: proxy.hostname,
-  port: proxy.port || 80,
-  path: target.href,
-  headers: {
-    "Proxy-Authorization": "Basic " + (new Buffer(proxy.auth).toString("base64")),
-    "Host" : target.hostname
-  }
-};
-
-http.get(options, function(res) {
-  res.pipe(process.stdout);
-  return console.log("status code", res.statusCode);
-  });
-//access an HTTP API
-
-http.(options, function(res)  {
-  var soap = require('soap');
-  // var wsdlurl = 'http://statutvolp.royalairmaroc.com/WebServiceStatutDeVol/services/FlightStatus?wsdl';
-  var args = {FlightNumber: 'AT424'};
-  var opts = {
-        wsdl_options: {
-            proxy: process.env.QUOTAGUARDSTATIC_URL
-            }
-          }
-    soap.createClient(options.path, opts, function(err, client) {
-        client.FlightStatus.FlightStatusHttpSoap12Endpoint.SmsgetFlightInfoByFlightNumber(args, function(err, result) {
-            console.log(result);
-            if(result != null){
-              let jsreturn = result.return;
-              console.log(jsreturn);}
+// // access an HTTP API
+// var http, options, proxy, url;
+//
+// http = require("http");
+// url = require("url");
+//
+// let quota = process.env.QUOTAGUARDSTATIC_URL";
+//
+// proxy = url.parse(quota);
+// target  = url.parse("http://statutvolp.royalairmaroc.com/WebServiceStatutDeVol/services/FlightStatus?wsdl");
+//
+// options = {
+//   hostname: proxy.hostname,
+//   port: proxy.port || 80,
+//   path: target.href,
+//   headers: {
+//     "Proxy-Authorization": "Basic " + (new Buffer(proxy.auth).toString("base64")),
+//     "Host" : target.hostname
+//   }
+// };
+//
+// http.get(options, function(res) {
+//   res.pipe(process.stdout);
+//   return console.log("status code", res.statusCode);
+//   });
+// //access an HTTP API
+//
+// http.(options, function(res)  {
+//   var soap = require('soap');
+//   // var wsdlurl = 'http://statutvolp.royalairmaroc.com/WebServiceStatutDeVol/services/FlightStatus?wsdl';
+//   var args = {FlightNumber: 'AT424'};
+//   var opts = {
+//         wsdl_options: {
+//             proxy: process.env.QUOTAGUARDSTATIC_URL
+//             }
+//           }
+//     soap.createClient(options.path, opts, function(err, client) {
+//         client.FlightStatus.FlightStatusHttpSoap12Endpoint.SmsgetFlightInfoByFlightNumber(args, function(err, result) {
+//             console.log(result);
+//             if(result != null){
+//               let jsreturn = result.return;
+//               console.log(jsreturn);}
             //
             // let statut = jsreturn[0].statut;
             // console.log(statut);}
-        });
-    });
-});
+//         });
+//     });
+// });
 
 //////PART II
-// var soap = require('soap');
-// var url = 'C:/Users/pc/webhook/flightstatus.xml';
-// var args = {FlightNumber: 'AT424'};
-//   soap.createClient(url, function(err, client) {
-//       client.FlightStatus.FlightStatusHttpSoap11Endpoint.SmsgetFlightInfoByFlightNumber(args, function(err, result) {
-//           console.log(result);
-//           if(result != null){
-//           let jsreturn = result.return;
-//           console.log(jsreturn);
-//
-//           let statut = jsreturn[0].statut;
-//           console.log(statut);}
-//       });
-//   });
+var soap = require('soap');
+var url = './flightstatus.xml';
+var args = {FlightNumber: 'AT424'};
+var opts = {
+      wsdl_options: {
+          proxy: process.env.QUOTAGUARDSTATIC_URL
+          }
+        }
+  soap.createClient(url, opts, function(err, client) {
+     console.log('connected');
+      client.FlightStatus.FlightStatusHttpSoap11Endpoint.SmsgetFlightInfoByFlightNumber(args, function(err, result) {
+          console.log(result);
+          if(result != null){
+          let jsreturn = result.return;
+          console.log(jsreturn);
+
+          let statut = jsreturn[0].statut;
+          console.log(statut);}
+      });
+  });
 
 // 'use strict';
 //
