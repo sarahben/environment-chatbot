c83dad2980dd498938a01c0c0296b29bb5fe2d25
Sarah <sarah.benfdil@gmail.com>
2019-01-14 13:11:25 +0100

    error

--- index.js
+++ index.js
@@ -473,7 +473,7 @@ function handleApiAiAction(sender, action, responseText, contexts, parameters, t
         sendTextMessage(sender.id, responseText);
       break;
       case "booking_info":
-      const fs = require('fs');
+      // const fs = require('fs');
       const path = require('path');
       const filePath = path.join(__dirname, 'xml.xml');
 
